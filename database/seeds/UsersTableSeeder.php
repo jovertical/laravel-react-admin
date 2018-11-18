b71a6f26dce3ac8341acbebc1a793cd2c0c9e51d
Jovert Palonpon <j.palonpon@iparentportal.com>
2018-11-18 19:00:11 +0800

    schema initial setup

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->type = 'superuser';
+        $user->username = 'jovert123';
+        $user->email = 'jovert@example.com';
+        $user->password = bcrypt('secret');
+
+        $user->firstname = 'Jovert';
+        $user->middlename = 'Lota';
+        $user->lastname = 'Palonpon';
+        $user->gender = 'male';
+        $user->birthdate = '1998-05-18';
+        $user->address = 'Marungko, Angat, Bulacan';
+        $user->save();
+
+        $user = new User;
+        $user->type = 'superuser';
+        $user->username = 'ian123';
+        $user->email = 'ian@example.com';
+        $user->password = bcrypt('secret');
+
+        $user->firstname = 'Ian';
+        $user->lastname = 'Lumbao';
+        $user->save();
+    }
+}
