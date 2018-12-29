8e1226ee0a855142b4c346beea1408166e26e2be
Jovert Palonpon <jovert.developer@gmail.com>
2018-12-29 15:36:28 +0800

    wip

--- resources/frontend/utils/ServiceWorker.js
+++ resources/frontend/utils/ServiceWorker.js
@@ -6,30 +6,8 @@ export function _register(serviceWorkerFile) {
         if (serviceWorkerFile) {
             navigator.serviceWorker
                 .register(`${serviceWorkerFile}`)
-                .then(() => {
-                    let deferredPrompt;
-
-                    window.addEventListener('beforeinstallprompt', e => {
-                        // Prevent Chrome 67 and earlier from automatically showing the prompt
-                        e.preventDefault();
-                        // Stash the event so it can be triggered later.
-                        deferredPrompt = e;
-                        // Update UI notify the user they can add to home screen
-                        btnAdd.style.display = 'block';
-                    });
-
-                    btnAdd.addEventListener('click', e => {
-                        // hide our user interface that shows our A2HS button
-                        btnAdd.style.display = 'none';
-                        // Show the prompt
-                        deferredPrompt.prompt();
-                        // Wait for the user to respond to the prompt
-                        deferredPrompt.userChoice.then(choiceResult => {
-                            deferredPrompt = null;
-                        });
-                    });
-                })
-                .catch(error => console.error(error));
+                .then(() => {})
+                .catch(error => {});
         }
     }
 }
