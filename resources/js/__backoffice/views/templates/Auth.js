aba983fe7ae2d51e44b1e357547771ee252e967b
Jovert Lota Palonpon <palonponjovertlota@gmail.com>
2019-03-06 17:09:27 +0800

    Updated logos

--- resources/js/__backoffice/views/templates/Auth.js
+++ resources/js/__backoffice/views/templates/Auth.js
@@ -9,7 +9,7 @@ import {
     withStyles,
 } from '@material-ui/core';
 
-import logo from '../../../../png/logos/2/960x540.png';
+import logo from '../../../../png/logos/1/512.png';
 
 const Auth = props => (
     <Grid
