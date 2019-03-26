59cb7e48bfafb57499b990c2a2bb6bdc249d615b
Jovert Lota Palonpon <palonponjovertlota@gmail.com>
2019-03-26 18:15:17 +0800

    wip

--- resources/js/routers/backoffice.js
+++ resources/js/routers/backoffice.js
@@ -6,31 +6,28 @@ export default [
         name: 'home',
         path: '/',
         component: Home,
-        auth: true,
     },
 
     {
         name: 'users.index',
         path: '/users',
         component: Users.List,
-        auth: true,
     },
 
     {
         name: 'users.create',
         path: '/users/create',
         component: Users.Create,
-        auth: true,
     },
 
     {
         name: 'users.edit',
         path: '/users/:id/edit',
         component: Users.Edit,
-        auth: true,
     },
 ].map(route => {
     route.name = `backoffice.${route.name}`;
+    route.auth = true;
 
     return route;
 });
